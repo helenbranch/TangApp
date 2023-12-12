0ec065e106a57fa73a30cbd20900ee088a3a9bf5
helenbranch <124572208+helenbranch@users.noreply.github.com>
2023-12-12 15:10:42 -0500

    lots of changes

---
+++ TangApp/Collection Cell /MyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionViewCell.swift
+//  TangApp
+//
+//  Created by helen branch on 11/21/23.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ TangApp/Table Cell/CollectionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionTableViewCell.swift
+//  TangApp
+//
+//  Created by helen branch on 11/21/23.
+//
+
+import UIKit
+
+class CollectionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
